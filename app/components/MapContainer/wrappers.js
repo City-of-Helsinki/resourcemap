76910ebabcb13030bea1f3824e30f6fb728af3e6
Evermade <evermade@antti.em87.io>
2018-11-01 14:34:20 +0000

    Change layout widths

--- app/components/MapContainer/wrappers.js
+++ app/components/MapContainer/wrappers.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-	width: 75%;
+	width: 80%;
 	display: flex;
 	height: 100%;
 	padding: 5% 1% 5% 5%;
--- app/components/Sidebar/wrappers.js
+++ app/components/Sidebar/wrappers.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 5% 5%;
-  width: 25%;
+  padding: 5% 2%;
+  width: 20%;
   text-align: center;
 `;
