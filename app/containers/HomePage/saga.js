163dae2b745500605c314d3a1eea41d1cce165f2
jaamo <jaakko@evermade.fi>
2018-11-29 12:14:25 +0200

    Append date parameter to backend query to get reservations for given day.

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -1,4 +1,5 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
+import dateFormat from 'dateformat';
 import { LOAD_RESOURCE } from './constants.js';
 import { loadResourceCompleted, loadResourceError } from './actions.js';
 import { makeSelectSpaces } from './selectors.js';
@@ -9,13 +10,21 @@ export function* loadResource() {
   // Get spaces.
   const spaces = yield select(makeSelectSpaces());
 
+  let start = new Date();
+  let startTimeStr = encodeURIComponent(
+    `${dateFormat(start, 'yyyy-mm-dd')}T00:00:00Z`,
+  );
+  let endTimeStr = encodeURIComponent(
+    `${dateFormat(start, 'yyyy-mm-dd')}T23:59:59Z`,
+  );
+
   // Load status of each space.
   for (let i = 0; i < spaces.size; i++) {
     const space = spaces.get(i);
     const id = space.get('id');
 
     if (id) {
-      const requestURL = `http://api.hel.fi/respa-test/v1/resource/${id}/`;
+      const requestURL = `http://api.hel.fi/respa-test/v1/resource/${id}/?start=${startTimeStr}&end=${endTimeStr}`;
 
       try {
         const resource = yield call(request, requestURL);
