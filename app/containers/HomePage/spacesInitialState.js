b0ec50f29a18e23dd860fbd38c00ccf62ab84a45
jaamo <jaakko@evermade.fi>
2018-12-11 22:41:01 +0200

    Added new space names and categories.

--- app/containers/HomePage/spacesInitialState.js
+++ app/containers/HomePage/spacesInitialState.js
@@ -1,165 +1,178 @@
 export default [
   {
-    category: 'Muut',
-    id: 'auzaluwo3xia',
-    name: '-',
-    available: 'taken',
-    d: 'M59.39,308.5 L59.39,385.76 L-0.93,385.76 L-0.93,308.5 L59.39,308.5 Z',
-  },
-  {
-    category: 'Ryhmätyö',
-    id: 'auungfvj7qba',
-    name: '-',
-    available: 'taken',
-    d: 'M139.86,327.5 L139.86,385.75 L64.5,385.75 L64.5,327.5 L139.86,327.5 Z',
-  },
-  {
-    category: 'Ryhmätyö',
-    id: 'auungcyhdraq',
-    name: '-',
+    category: 'Oppimistilat',
+    id: 'o1',
+    name: 'Oppimistila 1',
     available: 'available',
     d:
-      'M214.06,327.5 L214.06,409.77 L144.98,409.77 L144.98,327.5 L214.06,327.5 Z',
+      'M609.8,247.679487 L609.8,416.25 L543.27,416.25 L543.27,252.41293 L609.8,247.679487 ',
   },
   {
-    category: 'Ryhmätyö',
-    id: 'auungiaug77a',
-    name: '-',
+    category: 'Oppimistilat',
+    id: 'o2',
+    name: 'Oppimistila 2',
     available: 'available',
     d:
-      'M257.67,327.5 L257.67,395.41 L219.17,395.41 L219.17,327.5 L257.67,327.5 Z',
+      'M535.61,278.08 L535.61,368.75 L467.07,368.75 L467.07,278.08 L535.61,278.08 Z',
   },
   {
-    category: 'Musiikki ja video',
-    id: 'auungdysx6gq',
-    name: '-',
-    available: 'available',
-    d:
-      'M299.89,327.5 L299.89,395.41 L262.78,395.41 L262.78,327.5 L299.89,327.5 Z',
-  },
-  {
-    category: 'Ryhmätyö',
-    id: 'auungbhi4qvq',
-    name: '-',
-    available: 'available',
-    d:
-      'M377.64,324.5 L377.64,419.29 L302.01,419.29 L302.01,324.5 L377.64,324.5 Z',
-  },
-  {
-    category: 'Muut',
-    id: 'aurrojvvecua',
-    name: '-',
+    category: 'Oppimistilat',
+    id: 'o3',
+    name: 'Oppimistila 3',
     available: 'available',
     d:
       'M460.75,255.575382 L460.75,368.75 L381.36,368.75 L381.36,259.431082 L460.75,255.575382 Z',
   },
   {
-    category: 'Musiikki ja video',
-    id: 'aurrojvvaiiq',
-    name: '-',
-    available: 'soon',
-    d:
-      'M535.61,278.08 L535.61,368.75 L467.07,368.75 L467.07,278.08 L535.61,278.08 Z',
-  },
-  {
-    category: 'Muut',
-    id: 'aurrojvuydfq',
-    name: '-',
+    category: 'Oppimistilat',
+    id: 'av5k7ixqvzha',
+    name: 'Keittiö',
     available: 'available',
-    d:
-      'M609.8,247.679487 L609.8,416.25 L543.27,416.25 L543.27,252.41293 L609.8,247.679487 ',
+    d: 'M59.39,308.5 L59.39,385.76 L-0.93,385.76 L-0.93,308.5 L59.39,308.5 Z',
   },
 
   {
-    category: 'Kokoukset',
-    id: 'aurrojvuk6rq',
-    name: '-',
+    category: 'Ryhmätilat',
+    id: 'av5k4vxjxyha',
+    name: 'Ryhmätila 1',
     available: 'available',
     d: 'M620.22,-0.83 L620.22,67.5 L584.17,67.5 L584.17,-0.83 L620.22,-0.83 Z',
   },
   {
-    category: 'Kokoukset',
-    id: 'aurrojvt55dq',
-    name: '-',
-    available: 'soon',
+    category: 'Ryhmätilat',
+    id: 'av5k5ip53e2q',
+    name: 'Ryhmätila 2',
+    available: 'available',
     d: 'M662.21,-0.83 L662.21,67.5 L626.16,67.5 L626.16,-0.83 L662.21,-0.83 Z',
   },
   {
-    category: 'Kokoukset',
-    id: 'auy656ixq2eq',
-    name: '-',
-    available: 'soon',
-    d: 'M702.9,-0.83 L702.9,67.5 L668.15,67.5 L668.15,-0.83 L702.9,-0.83 Z',
-  },
-  {
-    category: 'Kokoukset',
-    id: 'au2miapu2tla',
-    name: '-',
+    category: 'Ryhmätilat',
+    id: 'av5k5ksofidq',
+    name: 'Ryhmätila 3',
     available: 'available',
     d: 'M801.36,-0.83 L801.36,67.5 L708.83,67.5 L708.83,-0.83 L801.36,-0.83 Z',
   },
   {
-    category: 'Kokoukset',
-    id: 'au2mt2xcytwq',
-    name: '-',
+    category: 'Ryhmätilat',
+    id: 'av5k5mveypsa',
+    name: 'Ryhmätila 4',
+    available: 'available',
+    d:
+      'M377.64,324.5 L377.64,419.29 L302.01,419.29 L302.01,324.5 L377.64,324.5 Z',
+  },
+  {
+    category: 'Ryhmätilat',
+    id: 'av5k57wpbdza',
+    name: 'Ryhmätila 5',
+    available: 'available',
+    d:
+      'M299.89,327.5 L299.89,395.41 L262.78,395.41 L262.78,327.5 L299.89,327.5 Z',
+  },
+  {
+    category: 'Ryhmätilat',
+    id: 'av5k6ayouzga',
+    name: 'Ryhmätila 6',
+    available: 'available',
+    d:
+      'M257.67,327.5 L257.67,395.41 L219.17,395.41 L219.17,327.5 L257.67,327.5 Z',
+  },
+  {
+    category: 'Ryhmätilat',
+    id: 'av5k6dfiu3va',
+    name: 'Ryhmätila 7',
+    available: 'available',
+    d:
+      'M214.06,327.5 L214.06,409.77 L144.98,409.77 L144.98,327.5 L214.06,327.5 Z',
+  },
+  {
+    category: 'Ryhmätilat',
+    id: 'av5k6eqxl4za',
+    name: 'Ryhmätila 8',
+    available: 'available',
+    d: 'M139.86,327.5 L139.86,385.75 L64.5,385.75 L64.5,327.5 L139.86,327.5 Z',
+  },
+  {
+    category: 'Ryhmätilat',
+    id: 'av5k6g75os4q',
+    name: 'Ryhmätila 9',
+    available: 'available',
+    d: 'M66.83,203.21 L66.83,252.5 L17.43,252.5 L17.43,203.21 L66.83,203.21 Z',
+  },
+  {
+    category: 'Ryhmätilat',
+    id: 'av5k6h56ne7q',
+    name: 'Ryhmätila 10',
     available: 'available',
     d: 'M66.83,147.2 L66.83,196.5 L17.43,196.5 L17.43,147.2 L66.83,147.2 Z',
   },
+
   {
-    category: 'Musiikki ja video',
-    id: 'au2mi3eomqqq',
-    name: '-',
-    available: 'soon',
-    d:
-      'M205.86,151.22 L205.86,198.2503 L110.946312,193.298371 L113.911999,151.22 L205.86,151.22 Z',
+    category: 'Kopiointi',
+    id: 'k',
+    name: 'Kopiointi',
+    available: 'available',
+    d: 'M702.9,-0.83 L702.9,67.5 L668.15,67.5 L668.15,-0.83 L702.9,-0.83 Z',
   },
+
   {
-    category: 'Musiikki ja video',
-    id: 'aurrojvrv7da',
-    name: '-',
-    available: 'soon',
-    d:
-      'M289.63,151.22 L289.63,198.439832 L213.37,201.562682 L213.37,151.22 L289.63,151.22 Z',
-  },
-  {
-    category: 'Musiikki ja video',
-    id: 'aurrojvrz2uq',
-    name: '-',
-    available: 'taken',
+    category: 'Studiot',
+    id: 's1',
+    name: 'Studio 1',
+    available: 'available',
     d:
       'M347.223505,166.5 L344.304526,203.353476 L296.5,198.618775 L296.5,166.5 L347.223505,166.5 Z',
   },
   {
-    category: 'Muut',
-    id: 'auunfzusy4xq',
-    name: '-',
+    category: 'Studiot',
+    id: 's2',
+    name: 'Studio 2',
     available: 'available',
     d:
-      'M1659.2,127.5 L1659.2,245.91 L1523.2,245.91 L1523.2,127.5 L1659.2,127.5 ',
+      'M289.63,151.22 L289.63,198.439832 L213.37,201.562682 L213.37,151.22 L289.63,151.22 Z',
   },
   {
-    category: 'Kokoukset',
-    id: 'auunfu22eqxq',
-    name: '-',
-    available: 'soon',
-    d: 'M66.83,203.21 L66.83,252.5 L17.43,252.5 L17.43,203.21 L66.83,203.21 Z',
+    category: 'Studiot',
+    id: 's3',
+    name: 'Studio 3',
+    available: 'available',
+    d:
+      'M205.86,151.22 L205.86,198.2503 L110.946312,193.298371 L113.911999,151.22 L205.86,151.22 Z',
   },
   {
-    category: 'Musiikki ja video',
-    id: 'auunfyouxlbq',
-    name: '-',
-    available: 'soon',
+    category: 'Studiot',
+    id: 's4',
+    name: 'Studio 4',
+    available: 'available',
     d:
       'M114.532366,202.683274 L114.286557,200.859627 L111.3,201.06 L111.3,202.513112 L114.532366,202.683274 Z M112.801579,201.06 L114.3,201.06 L114.3,201.09 L112.8,201.09 L112.801579,201.06 Z M112.801579,201.06 L112.878855,199.592074 L205.86,204.486888 L205.86,238.327362 L116.988888,243.36618 L111.313443,201.260373 L112.8,201.06 L112.801579,201.06 Z',
   },
   {
-    category: 'Musiikki ja video',
-    id: 'auungjalrhaq',
-    name: '-',
-    available: 'soon',
+    category: 'Studiot',
+    id: 's5',
+    name: 'Studio 5',
+    available: 'available',
+    d:
+      'M205.86,244.088878 L205.86,294.06 L113.091004,294.06 L117.060215,249.32881 L205.86,244.088878 Z',
+  },
+  {
+    category: 'Studiot',
+    id: 's6',
+    name: 'Studio 6',
+    available: 'available',
     d:
       'M240.513924,255 L214.14,255 L214.14,209.06104 L273.69,206.586371 L273.69,256.900696 L259.956315,270.345016 L258.920555,269.527341 L240.927094,255.322474 L240.513924,255 Z',
   },
+
+  {
+    category: 'Kuutio',
+    id: 'kuutio',
+    name: 'Kuutio',
+    available: 'available',
+    d:
+      'M1659.2,127.5 L1659.2,245.91 L1523.2,245.91 L1523.2,127.5 L1659.2,127.5 ',
+  },
+
+  /*
   {
     category: 'Musiikki ja video',
     id: 'auunf3jnfomq',
@@ -168,38 +181,32 @@ export default [
     d:
       'M285.74,209.819227 L285.74,206.73 L284.18,206.73 L284.09657,209.727678 L285.74,209.819227 Z M284.18,208.23 L284.24,208.23 L284.24,209.73 L284.18,209.73 L284.18,208.23 Z M284.18,208.23 L284.26343,206.732322 L344.713735,210.09977 L345.766202,266.448864 L295.652182,270.238737 L282.74,256.91767 L282.74,208.23 L284.18,208.23 Z',
   },
+  */
   {
-    category: 'Pelaaminen',
-    id: 'auur6zmfzh2a',
-    name: '-',
-    available: 'taken',
+    category: 'Pelitilat',
+    id: 'p1',
+    name: 'Pelitila 1',
+    available: 'available',
     d:
       'M428.7,227.531316 L381.36,229.421073 L381.36,193.26 L428.7,193.26 L428.7,227.531316 Z',
   },
   {
-    category: 'Pelaaminen',
-    id: 'auunf5rx6bea',
-    name: '-',
+    category: 'Pelitilat',
+    id: 'p2',
+    name: 'Pelitila 2',
     available: 'available',
     d:
       'M481.83,193.26 L481.83,225.531081 L434.92,227.411327 L434.92,193.26 L481.83,193.26 Z',
   },
   {
-    category: 'Pelaaminen',
-    id: 'avf57rzwwtsq',
-    name: '-',
+    category: 'Pelitilat',
+    id: 'p3',
+    name: 'Pelitila 3',
     available: 'available',
     d:
       'M535.61,193.26 L535.61,223.5316 L488.05,225.420765 L488.05,193.26 L535.61,193.26 Z',
   },
-  {
-    category: 'Musiikki ja video',
-    id: 'avf57v4lztpa',
-    name: '-',
-    available: 'available',
-    d:
-      'M205.86,244.088878 L205.86,294.06 L113.091004,294.06 L117.060215,249.32881 L205.86,244.088878 Z',
-  },
+  /*
   {
     category: 'Musiikki ja video',
     id: 'avf57xab6niq',
@@ -208,4 +215,5 @@ export default [
     d:
       'M238.184072,262.12 L256.6,277.137688 L256.6,294.06 L214.14,294.06 L214.14,262.12 L238.184072,262.12 Z',
   },
+  */
 ];
