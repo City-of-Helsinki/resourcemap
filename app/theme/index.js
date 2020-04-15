3b26af8eeefa7ec6695d93c366671221a2c541c9
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-04-15 12:35:05 +0300

    Use rem unit for font size
    This will make it easier to adjust the font size for bigger screens.

--- app/theme/index.js
+++ app/theme/index.js
@@ -1,6 +1,10 @@
-const fontSize = [10, 12, 15, 16, 18, 36, 45];
-fontSize.get = index => `${fontSize[index]}px`;
+export const ROOT_FONT_SIZE = 16; // px
+
+// With a root font size of 16px, these values equal to about the
+// following pixel values: [10, 12, 15, 16, 18, 36, 45]
+const fontSizeRem = [0.625, 0.75, 0.938, 1, 1.125, 2.25, 2.813];
+fontSizeRem.get = index => `${fontSizeRem[index]}rem`;
 
 export default {
-  fontSize,
+  fontSize: fontSizeRem,
 };
