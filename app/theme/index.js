02f567c117132ff9ddc3bfddec0ebe7350c9f02a
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-04-16 16:30:48 +0300

    Use colors from theme

--- app/theme/index.js
+++ app/theme/index.js
@@ -5,6 +5,19 @@ export const ROOT_FONT_SIZE = 16; // px
 const fontSizeRem = [0.625, 0.75, 0.938, 1, 1.125, 2.25, 2.813];
 fontSizeRem.get = index => `${fontSizeRem[index]}rem`;
 
+const colors = {
+  red: '#ed2127',
+  yellow: '#c2a251',
+  green: '#00b62b',
+  grey: '#5c5c5c',
+  blue: '#9fc9eb',
+};
+colors.noData = colors.grey;
+colors.taken = colors.red;
+colors.closed = colors.yellow;
+colors.available = colors.green;
+
 export default {
   fontSize: fontSizeRem,
+  colors,
 };
