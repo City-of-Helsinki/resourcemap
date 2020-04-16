f15a9a2ebdc13906f755c0046d700c675b03e5be
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-04-16 16:50:18 +0300

    Use pattern for taken rooms
    Using the pattern is important, because it allows red-green blind
    people to distinct an available room from a taken one.

--- app/components/Path/index.js
+++ app/components/Path/index.js
@@ -2,7 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import theme from 'theme';
 import './styles.css';
+
+// eslint-disable-next-line react/prop-types
+const TakenPattern = ({ id, color }) => (
+  <pattern
+    id={id}
+    width="3"
+    height="3"
+    patternTransform="rotate(-45 0 0)"
+    patternUnits="userSpaceOnUse"
+  >
+    <rect
+      width="3"
+      height="3"
+      style={{
+        fill: 'white',
+      }}
+    />
+    <line
+      x1="0"
+      y1="0"
+      x2="0"
+      y2="10"
+      style={{
+        stroke: color,
+        strokeWidth: 5,
+      }}
+    />
+  </pattern>
+);
+
+const TakenPatternClicked = () => (
+  <TakenPattern id="takenPatternClicked" color={theme.colors.taken} />
+);
+
+const TakenPatternHighlighted = () => (
+  <TakenPattern id="takenPatternHighlighted" color="#7b060a" />
+);
 
 const Path = ({ path, onClick, id, className }) => {
   const isString = typeof path === 'string';
@@ -13,12 +51,30 @@ const Path = ({ path, onClick, id, className }) => {
     className: classNames('roomPath', className),
     onClick: event => onClick(event, id),
   };
+  const isClicked = className.includes('clicked');
+  const isHighlighted = className.includes('is-highlighted');
+  const isTaken = className.includes('taken');
 
-  if (isString) {
-    return <path {...config} d={path} />;
-  }
+  const content = isString ? (
+    <path {...config} d={path} />
+  ) : (
+    React.createElement(path, config)
+  );
 
-  return React.createElement(path, config);
+  return (
+    <svg>
+      {(isClicked || isHighlighted) && isTaken && (
+        <defs>
+          {/* These patterns end up having duplicate ids. This is not 
+            ideal and it caused some inconsistent behaviour while 
+            developing. */}
+          {isClicked && <TakenPatternClicked />}
+          {isHighlighted && <TakenPatternHighlighted />}
+        </defs>
+      )}
+      {content}
+    </svg>
+  );
 };
 
 Path.propTypes = {
