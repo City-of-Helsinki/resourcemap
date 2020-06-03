8c80c1910ea01a05362372c4c1023a5ad308c4b5
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-06-03 11:52:02 +0300

    Use 15px as the smallest font size

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ### Changed
 - Use pattern instead of solid colour for taken rooms
+- Use a font sizes larger or as large as 15px
 
 ## [0.1.0] - 2020-04-16
 ### Added
--- app/theme/index.js
+++ app/theme/index.js
@@ -1,9 +1,10 @@
 export const ROOT_FONT_SIZE = 16; // px
 
 // With a root font size of 16px, these values equal to about the
-// following pixel values: [12, 12, 15, 16, 18, 36, 45]
-// Smallest font-size that's visible on the TV screen.
-const fontSizeRem = [0.75, 0.75, 0.938, 1, 1.125, 2.25, 2.813];
+// following pixel values: [15, 15, 15, 16, 18, 36, 45]
+// Smallest font-size that's visible on the TV screen is 15px according
+// to current knowledge.
+const fontSizeRem = [0.938, 0.938, 0.938, 1, 1.125, 2.25, 2.813];
 fontSizeRem.get = index => `${fontSizeRem[index]}rem`;
 
 const colors = {
